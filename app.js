a5f0d00304979b09d956fda2ad792d4281b8c958
Saloni Mathur <mathursaloni123@gmail.com>
2021-11-08 14:20:31 +0530

    added js
    added functionality for simple and hex using js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.querySelector("#btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click", function(){
+    const RandomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[RandomNumber];
+    color.textContent = colors[RandomNumber];
+})
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
